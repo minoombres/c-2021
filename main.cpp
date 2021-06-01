732ab4aa25e658f72b3e61accd670fd93a4b0f6b
minoombres <54647753+minoombres@users.noreply.github.com>
2021-06-01 10:40:58 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Nombre: Cristopher Alvaro Rios Salinas\n";
+	cout << "CU: 35-5150\n";
+	cout << "CI: 10409085\n";
+	cout << "Edad: 19 años";
+	return 0;
+}
